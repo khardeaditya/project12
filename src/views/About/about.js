f054edb5f75b5eea042c95d633902e2882a4f4f6
khardeaditya <khardeaditya240@gmail.com>
2024-09-01 22:14:54 -0700

    toaster add in about

--- src/views/About/about.js
+++ src/views/About/about.js
@@ -2,13 +2,24 @@ import React from 'react'
 import Nav from '../../component/Navbar/nav'
 import Foot from './../../component/footers/footer'
 import AboutImg from "./about.png"
+import toast, { Toaster } from 'react-hot-toast';
+
 function About() {
   return (
     <div>
       <Nav />
       <h2 className='title'>Abut Page</h2>
       <img src={AboutImg}  className='imgs'/>
+      <button type='button' onClick={    ()=>{
+        toast.loading("Loading....")
+      }
+     }>
+      Click Here
+      </button>
       <Foot />
+
+      <Toaster/>
+
     </div>
   )
 }
